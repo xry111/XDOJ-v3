b12144937477fe5cb0623758fbf2760622872e12
Xi Ruoyao <xry111@outlook.com>
2016-07-24 18:04:17 +0800

    [sandbox] Revert "Check kernel architecture"
    This reverts commit 912348354d4c2171759f41ebbf660e4eac3397c5.
    
    The description of that commit is totally wrong.

--- sandbox/reg_amd64.go
+++ sandbox/reg_amd64.go
@@ -1,4 +1,4 @@
-// getSyscall.go - Wrap sys_ptrace to get syscall ID
+// reg_amd64.go - x86-64 register specific
 // Copyright (C) 2016 Laboratory of ACM/ICPC, Xidian University
 
 // This is part of XDOJ-v3.
@@ -21,27 +21,4 @@
 
 package sandbox
 
-import (
-	"golang.org/x/sys/unix"
-	"syscall"
-	"unsafe"
-)
-
-// Wrap sys_ptrace to get syscall ID.
-func ptraceGetSyscall(pid int) (ID int, err error) {
-	// value from sys/reg.h, sys/ptrace.h
-	const PTRACE_PEEKUSER = 3
-	var id uintptr
-
-	// In kernel the API of PTRACE_PEEKUSER is strange.
-	// We have to wrap it like Glibc.  See ptrace(2).
-	r1, _, e1 := unix.RawSyscall6(unix.SYS_PTRACE,
-		PTRACE_PEEKUSER, uintptr(pid),
-		_REG_SYSCALL_ID*unsafe.Sizeof(id),
-		uintptr(unsafe.Pointer(&id)), 0, 0)
-
-	if r1 == ^uintptr(0) {
-		err = syscall.Errno(e1)
-	}
-	return int(id), err
-}
+const _REG_SYSCALL_ID = 15 // ORIG_RAX from sys/regs.h
